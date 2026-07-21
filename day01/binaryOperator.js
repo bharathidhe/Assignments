cb206391cadd901b9933c37545c47653e25396fc
bharathidhe <143255978+bharathidhe@users.noreply.github.com>
2026-07-21 15:54:24 +0530

    week1Day1 Assignments

---
+++ day01/binaryOperator.js
@@ -0,0 +1,18 @@
+//math--->  value1, value2 
+//arithematic operator -----> +,_,*, /, %
+
+var firstname = 'Hello'
+var secondname ='world'
+console.log(firstname+"  "+secondname)
+
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+console.log("201"+8)
+var a =3
+var b= 5
+console.log(a==b)
+console.log(a);
+
---
+++ day01/primitiveDataTypes.js
@@ -0,0 +1,22 @@
+
+
+var firstName ='DivyaBharathi'
+var companyName= "FIS"
+var mobileNumber =288373873
+var isAutomation = true
+var hasPlaywright 
+
+console.log(firstName)
+console.log (typeof firstName)
+
+console.log(companyName)
+console.log (typeof companyName)
+
+console.log(mobileNumber)
+console.log (typeof mobileNumber)
+
+console.log(isAutomation)
+console.log (typeof isAutomation)
+
+console.log(hasPlaywright)
+console.log (typeof hasPlaywright)
